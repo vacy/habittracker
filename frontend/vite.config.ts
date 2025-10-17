39669866eb8c5e80725a2c7383882c2a6ad68477
Jan-Florian Hilgenberg <florianhilgenberg@googlemail.com>
2025-10-17 10:29:11 +0200

    reconfigured build to have output directory public

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,9 @@ import packageJson from "./package.json" with { type: "json" }
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+  build: {
+    outDir: "./public",
+  },
   server: {
     open: true,
   },
