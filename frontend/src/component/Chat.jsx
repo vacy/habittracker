e8fe657172c3b66c0aaa90269b1322be31522193
Jan-Florian Hilgenberg <florianhilgenberg@googlemail.com>
2025-10-18 14:52:02 +0200

    cookie modifiations

--- frontend/src/component/Chat.jsx
+++ frontend/src/component/Chat.jsx
@@ -27,12 +27,14 @@ function Chat() {
   }, [])
 
   function fetchComments() {
-    fetch(apiHost + "/comments").then(response =>
-      response.json().then(comments => {
-        setData(comments)
-        setDataIsLoaded(true)
-      }),
-    )
+    fetch(apiHost + "/comments")
+      .then(response =>
+        response.json().then(comments => {
+          setData(comments)
+          setDataIsLoaded(true)
+        }),
+      )
+      .then(response => console.log("response is ok?: ", response.ok))
   }
 
   const postComment = event => {
