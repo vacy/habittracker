52e3b276d6f17efc09db84c2e6fe1cde526e9379
Jan-Florian Hilgenberg <florianhilgenberg@googlemail.com>
2025-10-18 11:50:48 +0200

    cookie modifiations

--- frontend/src/component/Login.jsx
+++ frontend/src/component/Login.jsx
@@ -25,19 +25,21 @@ function Login() {
         password: document.getElementById("password").value,
       }),
     }).then(response =>
-      response.text().then(response => {
+      response.then(response => {
         if (response.ok) {
-          const token = response.headers.get("x-bearer-token")
-          console.log(token)
-          let options = {
-            maxAge: 1000 * 60 * 60 * 10, // expire after 10 hours
-            // httpOnly: true, // Cookie will not be exposed to client side code
-            sameSite: "Strict", // If client and server origins are different
-            secure: true, // care about https
-          }
-          const cookies = new Cookies()
-          cookies.set("token", token, { options })
-          dispatch(fetchLoginStatusRequest())
+          response.text().then(response => {
+            const token = response.headers.get("x-bearer-token")
+            console.log(token)
+            let options = {
+              maxAge: 1000 * 60 * 60 * 10, // expire after 10 hours
+              // httpOnly: true, // Cookie will not be exposed to client side code
+              sameSite: "Strict", // If client and server origins are different
+              secure: true, // care about https
+            }
+            const cookies = new Cookies()
+            cookies.set("token", token, { options })
+            dispatch(fetchLoginStatusRequest())
+          })
         }
       }),
     )
